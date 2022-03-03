83248826e0d22b216eea65d9a7bcc326660520fe
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-03-03 09:34:40 +0300

    123

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
-	// Place your code here.
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
