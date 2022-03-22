6edba9dabe1b324f76451cfd0d9e5fadd794750c
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-03-22 11:09:22 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,61 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func IsInList(myRune rune, listRunes []rune) bool {
+	flag := false
+	for _, val := range listRunes {
+		if val == myRune {
+			flag = true
+			break
+		}
+	}
+	return flag
+}
+
+func Unpack(msg string) (string, error) {
+	var buf string
+	var isPrevEsc bool
+	resultString := ""
+
+	for id, val := range msg {
+		isPrevEsc = false
+		if id == 0 {
+			if unicode.IsDigit(val) {
+				return "", ErrInvalidString
+			}
+		}
+		if unicode.IsLetter(val) {
+			if len(buf) != 0 {
+				resultString += buf
+				buf = string(val)
+				continue
+			}
+			buf += string(val)
+			continue
+		}
+		if unicode.IsDigit(val) {
+			if isPrevEsc {
+				buf += string(val)
+				continue
+			}
+			if len(buf) == 0 {
+				return "", ErrInvalidString
+			}
+			resultString += strings.Repeat(buf, int(val-'0'))
+			buf = ""
+			continue
+		}
+		return "", ErrInvalidString
+	}
+
+	if len(buf) != 0 {
+		resultString += buf
+	}
+	return resultString, nil
 }
