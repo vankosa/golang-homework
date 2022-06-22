4fd9ed90e8ca0536d18bcee76f4696c4343e4275
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-06-22 16:46:40 +0300

    hw04_lru_cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -28,3 +28,56 @@ func NewCache(capacity int) Cache {
 		items:    make(map[Key]*ListItem, capacity),
 	}
 }
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	element, ok := c.items[key]
+	// if element doesn't exist
+	if !ok {
+		newItem := cacheItem{
+			key:   key,
+			value: value,
+		}
+		// add to queue
+		listItem := c.queue.PushFront(newItem)
+		if c.queue.Len() > c.capacity {
+			// get last element
+			lastElement := c.queue.Back()
+			// remove last element from queue
+			c.queue.Remove(lastElement)
+			// remove last element from items
+			lastItem := lastElement.Value.(cacheItem)
+			delete(c.items, lastItem.key)
+		}
+		// add to items
+		c.items[key] = listItem
+
+		return false
+	}
+	// replace value
+	item := element.Value.(cacheItem)
+	item.value = value
+	element.Value = item
+	// move element to front
+	c.queue.MoveToFront(element)
+	// update value in items
+	c.items[item.key] = element
+	return true
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	element, ok := c.items[key]
+	// if element doesn't exist
+	if !ok {
+		return nil, false
+	}
+	// move element to front
+	c.queue.MoveToFront(element)
+
+	item := element.Value.(cacheItem)
+	return item.value, true
+}
+
+func (c *lruCache) Clear() {
+	// clear cache
+	c.queue.MoveToFront(nil)
+}
