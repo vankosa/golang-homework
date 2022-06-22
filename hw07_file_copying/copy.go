c18b1c33870089dd8ddc61dd2dd19879730ceac9
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-06-22 16:59:03 +0300

    hw07_file_copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,62 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
-var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
-)
+var ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0o755)
+	if err != nil {
+		return err
+	}
+
+	fromFileInfo, err := fromFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if offset > fromFileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit > fromFileInfo.Size() {
+		limit = fromFileInfo.Size()
+	}
+
+	if limit == 0 {
+		limit = fromFileInfo.Size()
+	}
+
+	writer, err := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o755)
+	if err != nil {
+		return err
+	}
+
+	fromFile.Seek(offset, 0)
+	reader := io.LimitReader(fromFile, limit)
+
+	// start new bar
+	bar := pb.Full.Start64(limit)
+
+	// create proxy reader
+	barReader := bar.NewProxyReader(reader)
+
+	// copy from proxy reader
+	written, err := io.Copy(writer, barReader)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(written)
+
+	// finish bar
+	bar.Finish()
+
 	return nil
 }
