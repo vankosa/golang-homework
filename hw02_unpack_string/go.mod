f22d7a16d015680c88aa236eced98294f1c293cb
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-06-22 10:35:11 +0300

    hw02_unpack_string

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/vankosa/golang-homework/hw02_unpack_string
 
 go 1.16
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,49 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var (
+		result                    strings.Builder
+		previousRune, currentRune rune
+	)
+
+	for _, currentRune = range s {
+		if previousRune == 0 {
+			previousRune = currentRune
+			continue
+		}
+
+		if unicode.IsDigit(currentRune) {
+			if unicode.IsDigit(previousRune) {
+				return "", errors.New("invalid string")
+			}
+
+			count, err := strconv.Atoi(string(currentRune))
+			if err != nil {
+				return "", err
+			}
+
+			_, err = result.WriteString(strings.Repeat(string(previousRune), count))
+			if err != nil {
+				return "", err
+			}
+		} else if !unicode.IsDigit(previousRune) {
+			result.WriteString(string(previousRune))
+		}
+
+		previousRune = currentRune
+	}
+
+	if currentRune != 0 && !unicode.IsDigit(currentRune) {
+		result.WriteString(string(currentRune))
+	}
+
+	return result.String(), nil
 }
