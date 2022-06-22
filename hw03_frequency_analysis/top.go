e928eaff3d665443a8e655699f562acd9a04c68c
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-06-22 16:36:00 +0300

    hw03_frequency_analysis

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,35 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	wordsMap := map[string]int{}
+	words := strings.Fields(s)
+
+	for _, w := range words {
+		wordsMap[w]++
+	}
+
+	if len(words) == 0 {
+		return nil
+	}
+
+	keys := make([]string, 0, len(wordsMap))
+
+	for key := range wordsMap {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		if wordsMap[keys[i]] == wordsMap[keys[j]] {
+			return keys[i] < keys[j]
+		}
+
+		return wordsMap[keys[i]] > wordsMap[keys[j]]
+	})
+
+	return keys[:10]
 }
