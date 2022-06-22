36feeb587b0e6aa925f72a7f7bae2d10eb0bfc1e
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-06-22 16:24:00 +0300

    fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,42 +9,39 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(s string) (string, error) {
-	var (
-		result                    strings.Builder
-		previousRune, currentRune rune
-	)
+func Unpack(str string) (string, error) {
+	sr := []rune(str)
 
-	for _, currentRune = range s {
-		if previousRune == 0 {
-			previousRune = currentRune
+	var sb strings.Builder
+
+	for i, char := range sr {
+		if unicode.IsDigit(char) && i == 0 {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(char) && unicode.IsDigit(sr[i-1]) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(char) {
+			n, _ := strconv.Atoi(string(char))
+			if n == 0 {
+				tr := []rune(sb.String())
+				tr = tr[:len(tr)-1]
+
+				sb.Reset()
+				sb.WriteString(string(tr))
+
+				continue
+			}
+
+			repeater := strings.Repeat(string(sr[i-1]), n-1)
+			sb.WriteString(repeater)
 			continue
 		}
 
-		if unicode.IsDigit(currentRune) {
-			if unicode.IsDigit(previousRune) {
-				return "", errors.New("invalid string")
-			}
-
-			count, err := strconv.Atoi(string(currentRune))
-			if err != nil {
-				return "", err
-			}
-
-			_, err = result.WriteString(strings.Repeat(string(previousRune), count))
-			if err != nil {
-				return "", err
-			}
-		} else if !unicode.IsDigit(previousRune) {
-			result.WriteString(string(previousRune))
-		}
-
-		previousRune = currentRune
+		sb.WriteRune(char)
 	}
 
-	if currentRune != 0 && !unicode.IsDigit(currentRune) {
-		result.WriteString(string(currentRune))
-	}
-
-	return result.String(), nil
+	return sb.String(), nil
 }
