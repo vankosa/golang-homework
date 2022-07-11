8fc93bec5b23bdd24945ece2012c52721dbcbf2b
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-07-11 11:27:29 +0300

    test

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,81 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "connect timeout")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 2 {
+		log.Fatal("invalid arguments")
+	}
+
+	address := net.JoinHostPort(args[0], args[1])
+
+	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+	defer client.Close()
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	// receiver func
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				client.Close()
+
+				wg.Done()
+				return
+			default:
+				err := client.Receive()
+				if err != nil {
+					os.Exit(1)
+				}
+			}
+		}
+	}()
+
+	// sender func
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				client.Close()
+				wg.Done()
+				return
+			default:
+				err := client.Send()
+				if err != nil {
+					os.Exit(1)
+				}
+			}
+		}
+	}()
+
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -13,9 +18,69 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	return &NetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+type NetClient struct {
+	address string
+	conn    net.Conn
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	endMsg  string
+}
+
+func (c *NetClient) Connect() error {
+	var err error
+	c.conn, err = net.DialTimeout("tcp", c.address, c.timeout)
+	return err
+}
+
+func (c *NetClient) Send() error {
+	content := make([]byte, 100)
+
+	// get data from stdin
+	_, err := bufio.NewReader(c.in).Read(content)
+
+	// check EOF error
+	if errors.Is(err, io.EOF) {
+		c.endMsg = "...EOF"
+		return c.Close()
+	}
+	// try to write to connection
+	_, err = c.conn.Write(content)
+
+	// detect nc close connection
+	if _, ok := err.(*net.OpError); ok {
+		return c.Close()
+	}
+
+	return err
+}
+
+func (c *NetClient) Receive() error {
+	_, err := io.Copy(c.out, c.conn)
+
+	return err
+}
+
+func (c *NetClient) Close() error {
+	// set final word
+	endMsg := "...Connection was closed by peer"
+	if c.endMsg != "" {
+		endMsg = c.endMsg
+	}
+
+	// send final word to stderr
+	_, err := fmt.Fprintf(os.Stderr, endMsg)
+	if err != nil {
+		return err
+	}
+
+	return c.conn.Close()
+}
