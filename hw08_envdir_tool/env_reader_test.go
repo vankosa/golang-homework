d56bba5e8821081282585acbdf865a063fa5b4a6
Сазанов Иван Сергеевич <i.sazanov@dcoa.ru>
2022-07-11 12:07:41 +0300

    add new test

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("dir not found", func(t *testing.T) {
+		env, err := ReadDir("not found")
+		assert.Error(t, err)
+		assert.Len(t, env, 0)
+	})
+
+	t.Run("available tests", func(t *testing.T) {
+		env, err := ReadDir("./testdata/env")
+		assert.NoError(t, err)
+		assert.Len(t, env, 5)
+
+		envVal, ok := env["UNSET"]
+		assert.True(t, ok)
+		expected := EnvValue{
+			Value:      "",
+			NeedRemove: true,
+		}
+		assert.Equal(t, envVal, expected)
+	})
 }
